a830b0c086dae368e606c6b3b1ba29b825270ef8
Charly Otero <charlyotero92@gmail.com>
2024-03-05 23:27:54 -0300

    [Update] image wepb

---
+++ purgecss.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  content: ["index.html"], // Archivos HTML que contienen las clases CSS que deseas mantener
+  css: ["css/bootstrap.min.css"], // Archivos CSS que deseas purgar
+  output: "css/style.css", // Nombre del archivo de salida después del purgado
+};
